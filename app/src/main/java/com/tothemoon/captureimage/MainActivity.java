3b86f5e2d45ac2f121e3baac07243459bfe12f5c
Nathan Good <goodlnath@gmail.com>
2020-03-30 01:25:11 +0100

    0

--- app/src/main/java/com/tothemoon/captureimage/MainActivity.java
+++ app/src/main/java/com/tothemoon/captureimage/MainActivity.java
@@ -1,14 +1,34 @@
 package com.tothemoon.captureimage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonCaptureImage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(
+                        getApplicationContext(),
+                        Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED ||)
+            }
+        });
+    }
+
+    private void dispatchCaptureImageIntent() {
+
     }
 }
